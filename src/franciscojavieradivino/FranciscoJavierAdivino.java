be623f1dc590c02ea7488d694b9c7b05dba82548
Fran <Fran@DESKTOP-LO2QAGO>
2019-11-11 23:04:13 +0100

    Editamos la línea 11 para que los valores aleatorios generados sean entre 10 y 50.

--- src/franciscojavieradivino/FranciscoJavierAdivino.java
+++ src/franciscojavieradivino/FranciscoJavierAdivino.java
@@ -8,7 +8,7 @@ public class FranciscoJavierAdivino {
 
     public static void main(String[] args) {
          int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) (Math.random()*(10-50+1)+50);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
